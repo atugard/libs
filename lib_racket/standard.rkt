bfe8b79af55713167b2ccddcfa67257fdc85a0cd
David Draguta <daviddraguta@gmail.com>
2020-08-05 16:48:04 -0400

    Start working on implementing random number generator.

---
+++ lib_racket/standard.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require "mutable.rkt")
+
+(provide rand)
+
+(define (rand n)
+  (let ((already_seen null))
+    (define (loop)
+      (insert-mlist! n already_seen)
+      (define m
+        (middle-n-digits (* n n (expt 10 n))))
+      (set! n m))
+    (loop)))
+
+;;m=2n
+(define (middle-n-digits m)
+  (let ((sn (number->string n)))
+    (
+
+
+
+
+
+;;seed_number = int(input("Please enter a four digit number:\n[####] "))
+;;number = seed_number
+;;already_seen = set()
+;;counter = 0
+;;
+;;while number not in already_seen:
+;;    counter += 1
+;;    already_seen.add(number)
+;;    number = int(str(number * number).zfill(8)[2:6])  # zfill adds padding of zeroes
+;;    print(f"#{counter}: {number}")
+;;
+;;print(f"We began with {seed_number}, and"
+;;      f" have repeated ourselves after {counter} steps"
+;;      f" with {number}.")
