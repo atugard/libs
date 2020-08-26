6be7227fd32250dc0ac5aecd628764f2be5f5b2c
David Draguta <daviddraguta@gmail.com>
2020-08-26 12:43:13 -0400

    Added abs and sqrt functions.

--- lib_racket/math.rkt
+++ lib_racket/math.rkt
@@ -1,6 +1,6 @@
 #lang racket 
 
-(provide dec inc square)
+(provide dec inc square abs sqrt)
 
 (define (dec n)
   (- n 1))
@@ -8,3 +8,16 @@
   (+ n 1))
 (define (square n)
   (* n n))
+(define (abs n)
+  (if (>= n 0)
+      n
+      (- n)))
+(define (sqrt n)
+    (define (sqrt-iter lower upper tolerance)
+      (let ((middle (/ (+ lower upper) 2)))
+        (cond [(< (abs (- (square middle) n)) tolerance) middle]
+              [(< (square middle) n) (sqrt-iter middle upper tolerance)]
+              [else
+               (sqrt-iter lower middle tolerance)])))
+  (sqrt-iter 0.0 n .00000000001))
+             
