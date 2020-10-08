6f0f1e9059bbf471f8a8ee21625f1068100fd657
David Draguta <daviddraguta@gmail.com>
2020-10-08 09:19:12 -0400

    Push all local changes that I've forgot to push, lol.

--- lib_racket/functions.rkt
+++ lib_racket/functions.rkt
@@ -7,8 +7,8 @@
 
 (define identities
   (list
-   (cons '(+ (square cos) (square sin)) 1)
-   (cons '(+ (square sin) (square cos)) 1)))
+    (cons '(+ (square cos) (square sin)) 1)
+    (cons '(+ (square sin) (square cos)) 1)))
 
 ;;an idea for how to deal with this is... to keep track of the argument, and pass it to the identity, and see if its the same, in the case of a non-numeric result?
 ;;downside is having to evaluate all identities for each computed function... 
@@ -33,12 +33,12 @@
                  [(numbers? a1 a2)
                   (old+ a1 a2)]
                  [(procedures? a1 a2)
-                      (lambda x
-                        (let ((b1 (apply a1 x))
-                              (b2 (apply a2 x)))
-                          (if (numbers? b1 b2)
-                              (old+ b1 b2)
-                              (tag-list (list b1 b2) '+))))]
+                  (lambda x
+                    (let ((b1 (apply a1 x))
+                          (b2 (apply a2 x)))
+                      (if (numbers? b1 b2)
+                        (old+ b1 b2)
+                        (tag-list (list b1 b2) '+))))]
                  [(tuples? a1 a2)
                   ;;nyi
                   ]
@@ -53,15 +53,15 @@
                  [(and (number? a1) (procedure? a2))
                   (lambda x
                     (let ((b (apply a2 x)))
-                          (if (number? b)
-                              (old+ a1 b)
-                              (tag-list (list a1 b) '+))))]
+                      (if (number? b)
+                        (old+ a1 b)
+                        (tag-list (list a1 b) '+))))]
                  [(and (procedure? a1) (number? a2))
                   (lambda x
                     (let ((b (apply a1 x)))
-                          (if (number? b)
-                              (old+ a2 b)
-                              (tag-list (list a2 b) '+))))]
+                      (if (number? b)
+                        (old+ a2 b)
+                        (tag-list (list a2 b) '+))))]
                  [(and (procedure? a1) (tuple? a2)) ;;defined if dim(Im(a1)) = length(a2)
                   ;;nyi
                   ]
@@ -80,7 +80,7 @@
                       (and (number? a2) (pair? a1) (eq? (get-tag a1) '+)))
                   (tag-list (list a1 a2) '+)]))]
         [else
-         (+ (car args) (apply + (cdr args)))]))
+          (+ (car args) (apply + (cdr args)))]))
 
 ;;We have a_1 - a_2 - ... - a_n = a_1 + -a_2 + ... + -a_n
 (define (- . args)
@@ -94,19 +94,19 @@
                   (lambda x
                     (let ((b (apply a x)))
                       (if (number? b)
-                          (old- b)
-                          (list '- b))))]
+                        (old- b)
+                        (list '- b))))]
                  [(tuple? a)
                   ;;nyi
                   ]
                  [(and (tagged-list? a) (or (eq? (get-tag a) '+) (eq? (get-tag a) '*)))
                   (list '- a)]))]
         [else
-         (define (rec l)
-           (if (null? l)
-               null
-               (cons (- (car l)) (rec (cdr l)))))
-         (apply + (cons (car args) (rec (cdr args))))]))
+          (define (rec l)
+            (if (null? l)
+              null
+              (cons (- (car l)) (rec (cdr l)))))
+          (apply + (cons (car args) (rec (cdr args))))]))
 
 ;;a*b = a + a + a + ... + a <-- b times
 ;;a_1 * a_2 * ... * a_n = a_1 * (a_2 * ... * a_n) <-- can use recursion
@@ -153,8 +153,8 @@
                     (let ((b1 (apply a1 x))
                           (b2 (apply a2 x)))
                       (if (numbers? b1 b2)
-                          (old* b1 b2)
-                          (tag-list (list b1 b2) '*))))]
+                        (old* b1 b2)
+                        (tag-list (list b1 b2) '*))))]
                  [(and (procedure? a1) (symbol? a2))
                   (lambda x (list '* a2 (apply a1 x)))]
                  [(and (symbol? a1) (procedure? a2))
@@ -167,14 +167,14 @@
                   (lambda x
                     (let ((b (apply a1 x)))
                       (if (number? b)
-                          (old* b a2)
-                          (tag-list (list b a2) '*))))]
+                        (old* b a2)
+                        (tag-list (list b a2) '*))))]
                  [(and (procedure? a2) (number? a1))
                   (lambda x
                     (let ((b (apply a2 x)))
                       (if (number? b)
-                          (old* a1 b)
-                          (tag-list (list b a1) '*))))]
+                        (old* a1 b)
+                        (tag-list (list b a1) '*))))]
                  [(and (procedure? a1) (pair? a2) (eq? (get-tag a2) '*))
                   (lambda x (tag-list (list (apply a1 x) a2) '*))]
                  [(and (procedure? a2) (pair? a1) (eq? (get-tag a1) '*))
@@ -187,46 +187,46 @@
                       (and (number? a2) (pair? a1) (eq? (get-tag a1) '*)))
                   (tag-list (list a1 a2) '*)]))]
         [else
-         (* (car args) (apply * (cdr args)))]))
+          (* (car args) (apply * (cdr args)))]))
 
-                  
+
 (define (compose f g)
   (lambda (x)
     (f (g x))))
-  
+
 
 (define (expt a b)
   (if (or (not (number? a)) (not (number? b)))
-      (list 'expt a b)
-      (oldexpt a b)))
+    (list 'expt a b)
+    (oldexpt a b)))
 (define (exp a b)
   (if (or (not (number? a)) (not (number? b)))
-      (list 'exp a b)
-      (oldexp a b)))
+    (list 'exp a b)
+    (oldexp a b)))
 (define (sin a)
   (if (not (number? a))
-      (list 'sin a)
-      (oldsin a)))
+    (list 'sin a)
+    (oldsin a)))
 (define (cos a)
   (if (not (number? a))
-      (list 'cos a)
-      (oldcos a)))
+    (list 'cos a)
+    (oldcos a)))
 (define (tan a)
   (if (not (number? a))
-      (list 'tan a)
-      (oldtan a)))
+    (list 'tan a)
+    (oldtan a)))
 (define (sqrt a)
   (if (not (number? a))
-      (list 'sqrt a)
-      (oldsqrt a)))
+    (list 'sqrt a)
+    (oldsqrt a)))
 (define (square a)
   (* a a)) 
 (define (cube a)
   (* a a a))
 (define (factorial n)
   (if (= n 0)
-      1
-      (* n (factorial (- n 1)))))
-      
+    1
+    (* n (factorial (- n 1)))))
+
 
 
--- lib_racket/math.rkt
+++ lib_racket/math.rkt
@@ -1,6 +1,6 @@
-#lang racket 
-
-(provide dec inc square abs sqrt)
+;;#lang racket 
+;;
+;;(provide dec inc square abs sqrt sum product derivative definite-integral indefinite-integral)
 
 (define (dec n)
   (- n 1))
@@ -10,14 +10,70 @@
   (* n n))
 (define (abs n)
   (if (>= n 0)
-      n
-      (- n)))
+    n
+    (- n)))
 (define (sqrt n)
-    (define (sqrt-iter lower upper tolerance)
-      (let ((middle (/ (+ lower upper) 2)))
-        (cond [(< (abs (- (square middle) n)) tolerance) middle]
-              [(< (square middle) n) (sqrt-iter middle upper tolerance)]
-              [else
-               (sqrt-iter lower middle tolerance)])))
+  (define (sqrt-iter lower upper tolerance)
+    (let ((middle (/ (+ lower upper) 2)))
+      (cond [(< (abs (- (square middle) n)) tolerance) middle]
+            [(< (square middle) n) (sqrt-iter middle upper tolerance)]
+            [else
+              (sqrt-iter lower middle tolerance)])))
   (sqrt-iter 0.0 n .00000000001))
-             
+
+(define (range n)
+  (define (generate k)
+    (if (eq? k n)
+      null
+      (cons k (generate (+ k 1)))))
+  (generate 0))
+
+(define (definite-integral f)
+  (lambda (a b)
+    (let ((_a (* a 1.0))
+          (_b (* b 1.0)))
+      ;;split interval [a,b] into [t_0, t_1], ..., [t_n-1, t_n], where t_0=a, t_n=b, and t_{i+1}-t_{i}=b-a/n,
+      ;;so t_i = i(b-a)/n + a 
+      ;;Our integral will be approximated by \sum_{i=0}^{n-1} f((t_i + t_i+1})/2)(b-a)/n
+      ;;we can find (t_i+t_{i+1})/2 = i(b-a)/n + (b-a)/2n, so our integral approximation is
+      ;;\sum_{i=0}^{n-1} f(i(b-a)/n + (b-a)/2n)(b-a)/n
+
+      (define (rec i n)
+        (if (eq? i n)
+          0
+          (let ((t_i  (+ (* i (/ (- _b _a) n)) (/ (- _b _a) (* 2 n)))))
+            (+ (f t_i) (rec (+ i 1) n)))))
+      (* (rec 0 10000) (/ (- _b _a) 10000)))))
+
+(define (indefinite-integral f)
+  (lambda (x)
+    (let ((_x (* x 1.0)))
+      (define (rec i n)
+        (if (eq? i n)
+          0
+          (let ((t_i  (+ (* i (/ (- _x 0) n)) (/ (- _x 0) (* 2 n)))))
+            (+ (f t_i) (rec (+ i 1) n)))))
+      (* (rec 0 10000) (/ (- _x 0) 10000)))))
+
+(define (derivative f)
+  (lambda (x)
+    (define (approx h)
+      (/ (- (f (+ x h)) (f x)) h))
+    (approx 0.000000001)))
+
+
+(define (sum numbers)
+  (apply + numbers))
+(define (product numbers)
+  (apply * numbers))
+(define (factorial n)
+  (product (cdr (range (+ n 1)))))
+
+;;Make n-dimensional vectors as coordinates
+;;square root approximation?
+
+
+
+
+
+
