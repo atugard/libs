be9bfb72671881c3c173536bb9db3914fa7f3519
David Draguta <daviddraguta@gmail.com>
2020-10-09 15:03:42 -0400

    Implement sgn.

--- lib_racket/permutations.rkt
+++ lib_racket/permutations.rkt
@@ -22,6 +22,29 @@
 (define (compose-permutation p1 p2)
   (lambda (x) (p1 (p2 x))))
 
+(define (sgn p)
+  (define (find-max n)
+    (if (not (= (p n) n))
+      (find-max (+ n 1))
+      n))
+  (define (loop i j n result)
+    (cond [(= i n) result]
+          [(< j n)
+           (if (< (p j) (p i))
+             (loop i (+ j 1) n (+ result 1))
+             (loop i (+ j 1) n result))]
+          [else
+            (loop (+ i 1) (+ i 2) n result)]))
+ (expt -1 (loop 0 1 (find-max 1) 0)))
+
+
+;;generate a list of all permutations of {1, 2, ..., n}
+(define (S_n n)
+  (display 'tbd))
+
+
+
 (define p (permutation 1 2 3 4))
 (define q (permutation 1 2 3))
 (define s (compose-permutation p q))
+
