84c3354c67dfa0332c7596311fe8f56977dd54cb
David Draguta <daviddraguta@gmail.com>
2020-10-09 13:19:29 -0400

    Implemented permutations as functions.

--- lib_racket/permutations.rkt
+++ lib_racket/permutations.rkt
@@ -1,79 +1,27 @@
 (include "types.rkt")
+(include "functions.rkt")
 ;;motivation for developing machinery to deal with permutations is to implement
 ;;general determinant formula for square matrices in vectors.rkt
 
-(define identity-transposition
-  '(transposition))
-(define (identity-transposition? a)
-  (equal? identity-transposition))
+(define (transposition a b)
+  (lambda (x) 
+    (cond [(= x a) b]
+          [(= x b) a]
+          [else x])))
+(define (compose-transposition t1 t2)
+  (lambda (x)
+    (t1 (t2 x))))
 
-;;Develop transposition level machinery, and use it to recursively define higher order permutation machinery.
-(define (transposition . nums )
-  (cond [(null? nums) identity-transposition]
-        [(and (number-list? nums) (= (length nums) 2)) (cons 'transposition nums)]
-        [else
-          (error "transposition takes either no arguments or two arguments of type number. you gave: " nums)]))
-(define (transposition? t)
-  (or (not (null? t)) 
-      (and (eq? 'transposition (car t)) 
-           (or (= (length (cdr t)) 0 ) (= (length (cdr t) 2))))))
-(define (transposition-list? ts)
-  (type-list? ts transposition?))
+(define (permutation . nums)
+  (define (nums->transpositions _nums)
+    (if (null?  (cdr _nums))
+      null
+      (cons (transposition (car _nums) (cadr _nums))  (nums->transpositions (cdr _nums)))))
+  (apply compose (nums->transpositions nums)))
 
+(define (compose-permutation p1 p2)
+  (lambda (x) (p1 (p2 x))))
 
-;;We have sig = t_n t_{n-1} ... t_0. Transpositions are not commutable. 
-;;I'll adopt composition right  to left to match the way its written in math, so
-;;sig = compose-tranpose(t_n t_{n-1} ... t_0)
-;;compose-transpose will be defined recursively, with base case of two transposes.
-(define (transpose->function t)
-  (lambda (num)
-    (let ((nums (cdr t)))
-      (if (= (car nums) num)
-        (cadr nums)
-        (if (= (cadr nums) num)
-          (car nums)
-          num)))))
-
-(define (equal-transposition? t1 t2)
-  (let ((_t1 (cdr t1))
-        (_t2 (cdr t2)))
-    (or (equal? _t1 _t2) (and (equal? (car _t1) (cadr _t2)) (equal? (car _t2) (cadr _t1))))))
-
-(define (permutation . params)
-  (define (num-occurences t ts)
-    (define (iter i ts)
-      (cond [(null? ts) i]
-            [(equal-transposition? (car ts) t) (iter (+ i 1) (cdr ts))]
-            [else 
-              (iter i (cdr ts))]))
-    (iter 0 ts))
-  (define (pre . params)
-    (cond [(number-list? params)
-           (define (nums->transpositions nums)
-             (define (rec-decomp ns)
-               (if (null? (cdr ns))
-                 null
-                 (cons (transposition (car ns) (cadr ns)) (rec-decomp (cdr ns)))))
-             (rec-decomp nums))
-           (nums->transpositions params)]
-          [(transposition-list? params) params]
-          [else
-            (error "permutation is an n-ary operator that either takes all arguments of type number or all arguments of type transposition. you gave: " params)]))
-  (define (filter-duplicates t occurences ts)
-    (cond  [(= occurences 1) ts]
-           [(= (remainder occurences 2) 0) 
-            (filter (lambda (x) (not (equal-transposition? x t))) ts)]
-           [(> occurences 1)
-            (cons t (filter (lambda (x) (not (equal-transposition? x t))) ts))]))
-  (define (loop ts)
-    (define (iter _ts result)
-      (if (null? _ts)
-        result
-        (iter (cdr _ts) (filter-duplicates (car _ts) (num-occurences (car _ts) result) result))))
-    (iter ts ts))
-  (cons 'permutation (loop (apply pre params))))
-
-
-
-
-(define p (permutation 5 4 3 2 1))
+(define p (permutation 1 2 3 4))
+(define q (permutation 1 2 3))
+(define s (compose-permutation p q))
