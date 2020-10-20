58e2e292038d46c20c19037e6bbb3e138bdce895
David Draguta <daviddraguta@gmail.com>
2020-10-20 17:34:47 -0400

    Start implementing trees.

---
+++ racket/trees.rkt
@@ -0,0 +1,11 @@
+;;We could define trees recursively. T1 = (T2, T3), where those two are one unit shorter, and so on, until we hit leaves, and then use those leaves to make our tree, and so on. 
+(define (tree r . subtrees)
+  )
+
+
+
+;;(a+b)^p. A choice tree of height p gives you all the info to compute this. 
+
+
+
+
