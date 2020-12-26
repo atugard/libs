162be7f8cbb0062bf941c4cb8e96a993acc7df25
David Draguta <daviddraguta@gmail.com>
2020-12-26 15:28:51 -0500

    Make basic sort module. Only has quicksort so far.

---
+++ haskell/Sort.hs
@@ -0,0 +1,9 @@
+module Sort where
+
+quicksort :: Ord a => [a] -> [a]
+quicksort []     = []
+quicksort (p:xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+    where
+        lesser  = filter (< p) xs
+        greater = filter (>= p) xs
+
