0ae5a0bbf9e5387124441e14371601a55fddbdf6
David Draguta <daviddraguta@gmail.com>
2021-01-01 20:01:06 -0500

    Solve a few problems. Implement permutations, combinations, and various helper functions.

--- haskell/Math.hs
+++ haskell/Math.hs
@@ -1,4 +1,5 @@
-module Math (fac, gcdLC, inverseModuloN, derivative, definite_integral, integral, permutationsN, permutations) where 
+{-# LANGUAGE DefaultSignatures #-}
+module Math (fac, gcdLC, inverseModuloN, derivative, definite_integral, integral, permutationsN, permutations, collectionsN, collections) where 
 
 import           Data.Maybe
 import           Sort
@@ -9,12 +10,42 @@ fac 0 = 1
 fac n = n * fac (n - 1)
 
 
+(\\) :: (Eq a) => [a] -> [a] -> [a] 
+(\\) xs [] = xs 
+(\\) [] _  = [] 
+(\\) xs (y:ys) = (filter (/= y) xs) \\ ys 
+
+naiveSearch :: (Eq a) => [a] -> [a] -> Bool 
+naiveSearch [] [] = True 
+naiveSearch _  [] = False 
+naiveSearch [] _  = True
+naiveSearch (x:xs) (y:ys)
+  | length xs > length ys  = False 
+  | x == y                 = naiveSearch xs     ys
+  | otherwise              = False 
+
+foldr' :: ([a] -> [b] -> Bool) -> Bool -> [a] -> [b] -> Bool
+foldr' _ empt _ [] = empt
+foldr' op empt xs ys = op xs ys || foldr' op empt xs (tail ys)
+
+sublist :: (Eq a) => [a] -> [a] -> Bool 
+sublist xs ys = foldr' naiveSearch False xs ys
+
+
+-- returns true if all elements in xs are in ys, irrespective of how they are placed in the list. 
+inList :: (Eq a) => [a] -> [a] -> Bool 
+inList [] ys = True
+inList xs [] = False 
+inList (x:xs) ys = x `elem` ys && inList xs ys 
+
+
 filterDuplicates :: Ord a => [a] -> [a]
 filterDuplicates xs = f . filter (\(x, y) -> x /= y) $ zip oxs (tail oxs)
   where oxs          = quicksort xs
         f (y:ys)     = if null ys
                           then [fst y, snd y]
                           else fst y: f ys
+
 
 _gcdTrail :: Int -> Int -> [[Int]] -> [[Int]]
 _gcdTrail 0 _ res = res 
@@ -73,9 +104,9 @@ inverseModuloN 1 n = Just 1
 inverseModuloN a n 
   |gcd a n /= 1 = Nothing 
   | otherwise    = let d  = gcdLC a n 
-                    in if d !! 1 == n 
-                          then Just $ (d !! 2) `mod` n 
-                          else Just $ (d !! 0) `mod` n
+  in if d !! 1 == n 
+        then Just $ (d !! 2) `mod` n 
+        else Just $ (d !! 0) `mod` n
 
 derivative_approx :: Float -> (Float -> Float) -> Float -> Float 
 derivative_approx h f x = (f (x + h) - f x) / h
@@ -93,23 +124,108 @@ definite_integral = definite_integral_approx 10000
 integral :: (Float -> Float) -> Float -> Float 
 integral f x = definite_integral f 0 x
 
-_permutations :: (Ord a , Eq a) => [a] -> [a] -> [[a]]
-_permutations [] _  = [] 
-_permutations [x] _ = [[x]]
-_permutations _  [] = []
-_permutations xs (y:ys) =  ((:) <$> [y] <*> _permutations rest rest) ++ _permutations xs ys 
+_permutationsN :: (Ord a , Eq a) => Int -> [a] -> [a] -> [[a]]
+_permutationsN _ [] _  = [] 
+_permutationsN 0 _ _   = [] 
+_permutationsN _ _  [] = []
+_permutationsN 1 xs _  = copies 1 xs 
+_permutationsN k xs (y:ys) =  ((:) <$> [y] <*> _permutationsN (k-1) rest rest) ++ _permutationsN k xs ys 
   where rest = filter (/= y) xs 
 
-permutations :: (Ord a, Eq a) => [a] -> [[a]]
-permutations xs = _permutations xs xs 
+permutationsN :: (Ord a , Eq a) => Int -> [a] -> [[a]]
+permutationsN k xs = _permutationsN k xs xs 
 
-permutationsN :: (Eq a, Ord a) => Int -> [a] -> [[a]]
-permutationsN k xs = filterDuplicates . map (take k) . permutations $ xs 
+permutations :: (Ord a, Eq a) => [a] -> [[a]]
+permutations xs = permutationsN (length xs) xs
+--
+
+combinationsN :: (Ord a) => Int -> [a] -> [[a]]
+combinationsN 0 xs    = [[]]
+combinationsN _ []    = []
+combinationsN 1 xs    = copies 1 xs 
+combinationsN k (x:xs) = (map (x:) $ combinationsN (k-1) xs) ++ combinationsN k xs 
 
 --5. The numbers 1,2,..., are arranged randomly. Find the probability that the digits 
 --   1,2...,k (k<n) appear as neighbors in that order.
+--   Just filter list checking if a sublist exists. The sublist never changes. It's always [1,2,3,...,k]
+sol5 :: Int -> Int -> Float 
+sol5 k n = fnum / fdenom
+  where prms  = permutations [1..n]
+        num   = length $ filter (sublist [1..k]) prms 
+        fnum  = fromIntegral num :: Float 
+        denom = length prms 
+        fdenom = fromIntegral denom :: Float 
 
---sol5 :: Int -> Float 
---sol5 k n = map check zip perm 
---  where perm  = permutations [1..n]
---        check (x:xs) = 
+--6. A pin table has seven holes through wihch a ball can drop. Five balls are played. Assuming that at each play a ball is equally likely to go down any one of the 
+--   seven holes, find the probability that more than one ball goes down at least one of the holes. 
+--   I think its 1 - nCk/n^k, lets see if we can write a program that explicitly computes lists of the possible outcomes 
+
+copies :: (Eq a, Ord a) => Int -> [a] -> [[a]] 
+copies k xs = map (replicate k) xs 
+
+collectionsN :: (Eq a, Ord a) => Int -> [a] -> [[a]]
+collectionsN 0 _ = [] 
+collectionsN 1 xs = quicksort $ copies 1 xs 
+collectionsN k xs = quicksort $ concatMap (\x -> map (x:) $ collectionsN (k-1) xs) xs  
+
+collections :: (Eq a, Ord a) => [a] -> [[a]]
+collections xs = collectionsN (length xs) xs 
+
+--eqList [a_1,...,a_n] == True if a_i == a_j for all i,j, else False.
+eqList :: (Eq a) => [a] -> Bool 
+eqList [] = True 
+eqList [x] = True 
+eqList (x:y:xs) = x == y && eqList (y:xs)
+
+repetitions :: (Ord a, Eq a) => [a] -> Bool 
+repetitions xs =  or . map (\(x,y) -> x == y) $ zip oxs (tail oxs)
+  where oxs = quicksort xs 
+
+sol6 :: Int -> Int -> Float 
+sol6 k n = twoOrMoreF / allF
+  where        
+        cks        = collectionsN k [1..n]
+        twoOrMore  = length $ filter repetitions cks
+        all        = length $ cks
+        twoOrMoreF = fromIntegral twoOrMore :: Float 
+        allF       = fromIntegral all :: Float 
+
+--Yeah, the answer is 1 - nCk/n^k, with k=5, n=7... one can explicitly check that with sol6 5 7.
+
+-- 7. If 2n boys are divided into two equal subgroups find the probability that the two tallest boys will be (a) 
+--    in different subgroups and (b) in the same subgroup.
+sol7 :: Int -> (Float, Float)
+sol7 n = (num1/den, num2/den)
+  where xs = map (splitAt n) $ permutations [1..2*n]
+        pred1 x = inList [1,2] x 
+        pred2 (x,y) = not (pred1 x) && not (pred1 y)
+        num1 = fromIntegral ((*2) . length . filter pred1 . map fst $ xs) :: Float 
+        num2 = fromIntegral (length $ filter pred2 xs) :: Float 
+        den = fromIntegral (length xs) :: Float 
+
+--The combinatorics gives fst (sol7 n) = n-1 / (2n-1).
+--                        snd (sol7 n) = 1 - (fst (sol7 n)) = n/(2n-1)
+--which matches sol7's output.
+--filter (\(x,y) -> (not (inList [1,2] x)) && (not (inList [1,2] y))) . map (splitAt 2) . permutations $ [1..4]
+
+-- 8. In a movie theater that can accomodate n + k people, n people are seated. What is the probability that r <= n given seats are occupied. 
+sol8_1 :: Int -> Int -> Int -> Float 
+sol8_1 r n k = num/den
+  where nums   = [1..n+k]
+        cxs    = combinationsN n nums 
+        guess  = filter (sublist [1..r]) cxs 
+        num    = fromIntegral (length guess) :: Float 
+        den    = fromIntegral (length cxs)   :: Float 
+
+seats :: Int -> Int -> [([Int], [Int])]
+seats n k = map (\x -> (x, nums \\ x)) $ combinationsN n nums
+  where nums = [1..n+k]
+
+--Turns out that this can be represented by nCr/(n+k)Cr, which prompted another way to do this
+sol8_2 :: Int -> Int -> Int -> Float 
+sol8_2 r n k = num/den
+  where cxs = permutationsN r [1..n] 
+        pxs = permutationsN r [1..n+k]
+        num = fromIntegral (length cxs) :: Float 
+        den = fromIntegral (length pxs) :: Float 
+
